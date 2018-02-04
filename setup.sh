e298760402f9948cd9b754376add795b125fdb1d
Damien Nugent <damien@damiennugent.com>
2018-02-04 14:59:55 +0000

    Setup for the most part works now. Packages work completely. dotfiles still requires work
    Further updates incoming

--- setup.sh
+++ setup.sh
@@ -36,9 +36,10 @@ command_line_tools=$(softwareupdate -l |
 
 
 if [[ -x "/usr/bin/ssh-keygen" ]]; then
-  if [[ -f "$ssh_key_loc" ]]; then
+  if [[ ! -f "$ssh_key_loc" ]]; then
     echo "Generating New SSH Key - You will be prompted for a password"
-    /usr/bin/ssh-keygen -t rsa -N
+    /usr/bin/ssh-keygen -t rsa 
+    ssh-add
   else
     echo "SSH Key found: $(ls "$ssh_key_loc")"
   fi 
@@ -49,11 +50,16 @@ EOS
 fi
 
 if [[ -x "/usr/bin/xcode-select" ]]; then
-##  echo "Installing xcode command line tools - expect a GUI prompt"
-##  /usr/bin/xcode-select --install
-  echo "Installing Xcode Command Line Tools - via software update"
-  touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-  /usr/sbin/softwareupdate -i "$command_line_tools" -v
+  echo "Installing xcode command line tools - expect a GUI prompt"
+  /usr/bin/xcode-select --install
+
+#  echo "Installing Xcode Command Line Tools - via software update"
+#  touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+#  /usr/sbin/softwareupdate -i "$(softwareupdate -l |
+#  grep "\*.*Command Line" |
+#  head -n 1 | awk -F"*" '{print $2}' |
+#  sed -e 's/^ *//' |
+#  tr -d '\n')" --verbose
 else 
   odie <<EOS
 Install xcode + confirm xcode-select is present in /usr/bin/
@@ -62,15 +68,21 @@ fi
 
 if [[ -x "/usr/bin/easy_install" ]]; then
   echo "Installing Ansible"
-  /usr/bin/easy_install ansible
+  sudo /usr/bin/easy_install pip
+  pip install --user ansible
+   if [[ $? -eq 1 ]]; then
+    odie <<EOS
+Failed to install Ansible
+EOS
+    fi
 else
   odie <<EOS
 easy_install not present - D:
 EOS
 fi
 
-if [[ $(ping -c 1 raw.githubusercontent.com) -eq 0 ]]; then
-  if [[ $(ping -c 1 github.com) -eq 0 ]]; then
+if [[ $(ping -c 1 raw.githubusercontent.com > /dev/null 2&>1) -eq 0 ]]; then
+  if [[ $(ping -c 1 github.com > /dev/null 2&>1) -eq 0 ]]; then
     echo "Installing Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
@@ -84,19 +96,19 @@ Unable to successfully ping 'raw.githubusercontent.com' to pull down the brew in
 EOS
 fi
 
-echo "Starting SSH to run Ansible"
-sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+#echo "Starting SSH to run Ansible"
+#sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
 echo "installing useful packages from brew"
-/usr/local/bin/ansible-playbook -i hosts packages.yml
+/usr/local/bin/ansible-playbook packages.yml
 echo "Pulling Dotfiles from git"
-/usr/local/bin/ansible-playbook -i hosts dotfiles.yml
-sudo launchctl unload -w /System/Library/LaunchDaemons/ssh.plist
+/usr/local/bin/ansible-playbook dotfiles.yml
+#sudo launchctl unload -w /System/Library/LaunchDaemons/ssh.plist
 
 echo "Adjusting NSGlobalDomain Settings"
 /usr/bin/defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1 ### Sidebar Icon size
 
 echo "Adjusting Finder settings"
-/usr/bin/defaults write com.apple.finder AppleShowAllFiles = YES
+/usr/bin/defaults write com.apple.finder AppleShowAllFiles -bool true
 /usr/bin/defaults write com.apple.finder ShowStatusBar -bool true
 /usr/bin/defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 /usr/bin/defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
@@ -104,4 +116,4 @@ echo "Adjusting Finder settings"
 /usr/bin/defaults write com.apple.finder DisableAllAnimations -bool true
 
 echo "Restarting Finder"
-killall "Finder" >/dev/null 2>&1
+killall "Finder" > /dev/null 2>&1
